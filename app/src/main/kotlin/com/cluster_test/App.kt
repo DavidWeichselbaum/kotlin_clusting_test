c84e3f05ff5a18d5ce05a882355628e73436059e
David T. Weichselbaum <david.t.weichselbaum@gmail.com>
2023-08-02 22:59:24 +0200

    Fix clustering

--- app/src/main/kotlin/com/cluster_test/App.kt
+++ app/src/main/kotlin/com/cluster_test/App.kt
@@ -6,12 +6,14 @@ class App {
     val data = arrayOf(
         doubleArrayOf(1.0, 2.0, 3.0),
         doubleArrayOf(4.0, 5.0, 6.0),
-        doubleArrayOf(7.0, 8.0, 9.0)
+        doubleArrayOf(7.0, 8.0, 9.0), 
+        doubleArrayOf(1.0, 8.0, 3.0), 
+        doubleArrayOf(7.0, 8.0, 9.0), 
+        doubleArrayOf(7.0, 8.0, 9.0), 
     )
 
-    val k = 2.0
-    val y = IntArray(data.size)
-    val kmeans = KMeans(k, data, y)
+    val k = 2
+    val kmeans = KMeans.fit(data, k)
 }
 
 fun main() {
