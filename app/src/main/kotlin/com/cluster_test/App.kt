a45946e4513e1fb4b0555d17bf3078060934d0e1
David T. Weichselbaum <david.t.weichselbaum@gmail.com>
2023-08-02 23:25:26 +0200

    Add hierarchical custering

--- app/src/main/kotlin/com/cluster_test/App.kt
+++ app/src/main/kotlin/com/cluster_test/App.kt
@@ -1,21 +1,25 @@
 package com.cluster_test
 
-import smile.clustering.KMeans
+import smile.clustering.linkage.CompleteLinkage
+import smile.clustering.HierarchicalClustering
+import smile.math.distance.EuclideanDistance
 
 class App {
-    val data = arrayOf(
-        doubleArrayOf(1.0, 2.0, 3.0),
-        doubleArrayOf(4.0, 5.0, 6.0),
-        doubleArrayOf(7.0, 8.0, 9.0), 
-        doubleArrayOf(1.0, 8.0, 3.0), 
-        doubleArrayOf(7.0, 8.0, 9.0), 
-        doubleArrayOf(7.0, 8.0, 9.0), 
+    val similarities = arrayOf(
+        doubleArrayOf(1.0, 2.0, 3.0, 4.0),
+        doubleArrayOf(4.0, 5.0, 6.0, 7.0),
+        doubleArrayOf(7.0, 8.0, 9.0, 10.0), 
+        doubleArrayOf(11.0, 12.0, 13.0, 14.0),
     )
 
-    val k = 2
-    val kmeans = KMeans.fit(data, k)
+    val linkage = CompleteLinkage.of(similarities)
+    val hclust = HierarchicalClustering.fit(linkage)
 }
 
 fun main() {
-    println(App().kmeans)
+    // println(App().hclust.tree())
+    val tree = App().hclust.tree()
+    for (row in tree) {
+        println(row.joinToString(", "))
+    }
 }
